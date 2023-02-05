dd3119775e017ba5d99bb36d8709670420711b6c
Daffa Aldzakian F <daffaaldzakian20@gmail.com>
2023-02-06 00:04:53 +0700

    finish the #1 challenge

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import CardImg from './img/image-qr-code.png'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card">
+        <img src={CardImg} alt='' />
+
+        <div className="cardBody">
+          <h2>Improve your front-end skill by building projects</h2>
+          <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+        </div>
+      </div>
     </div>
   );
 }
