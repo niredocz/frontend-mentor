98b6816ced75275e7ae3c9455c5432ce85c13200
Daffa Aldzakian F <daffaaldzakian20@gmail.com>
2023-02-06 00:20:38 +0700

    upload scan qr code project

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import './App.css';
-import CardImg from './img/image-qr-code.png'
+import { Route, Routes } from 'react-router-dom'
+import ScanQR from './projects/scan-qr-code/App'
 
 
 function App() {
   return (
     <div className="App">
-      <div className="card">
-        <img src={CardImg} alt='' />
-
-        <div className="cardBody">
-          <h2>Improve your front-end skill by building projects</h2>
-          <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
-        </div>
-      </div>
+      <Routes>
+        <Route path='/' element={<ScanQR />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/projects/scan-qr-code/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import CardImg from '../../img/image-qr-code.png'
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="card">
+        <img src={CardImg} alt='' />
+
+        <div className="cardBody">
+          <h2>Improve your front-end skill by building projects</h2>
+          <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
