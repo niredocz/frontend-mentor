14796a8fd0b8302bcbdf8543e26f2ef425d417e1
Daffa Aldzakian F <daffaaldzakian20@gmail.com>
2023-02-06 12:16:30 +0700

    add advice gen new route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom'
-import ScanQR from './projects/scan-qr-code/App'
+import ScanCode from './projects/scan-qr-code/ScanCode'
+import AdviceGenerator from './projects/advice-generator/AdviceGenerator'
 
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path='/scan-qr-code' element={<ScanQR />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path='/scan-qr-code' element={<ScanCode />} />
+      <Route path='/advice-generator' element={<AdviceGenerator />} />
+    </Routes>
   );
 }
 
