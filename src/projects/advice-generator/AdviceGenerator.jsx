30256a7071da8c75ca128e0c99413bfd594b136b
Daffa Aldzakian F <daffaaldzakian20@gmail.com>
2023-02-06 13:50:01 +0700

    switch js file into jsx format

---
+++ src/projects/advice-generator/AdviceGenerator.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./AdviceGenerator.css";
+
+const App = () => {
+  const numAdvice = 117;
+  return (
+    <div className="App bgAdvice">
+      <div className="card">
+        <span>ADVICE #{numAdvice}</span>
+      </div>
+    </div>
+  );
+};
+
+export default App;
--- src/projects/scan-qr-code/ScanCode.jsx
+++ src/projects/scan-qr-code/ScanCode.jsx
@@ -1,15 +1,18 @@
-import './ScanCode.css';
-import CardImg from '../../img/image-qr-code.png'
+import "./ScanCode.css";
+import CardImg from "./image-qr-code.png";
 
 function App() {
   return (
     <div className="App bgScan">
       <div className="card">
-        <img src={CardImg} alt='' />
+        <img src={CardImg} alt="" />
 
         <div className="cardBody">
           <h2>Improve your front-end skill by building projects</h2>
-          <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+          <p>
+            Scan the QR code to visit Frontend Mentor and take your coding
+            skills to the next level
+          </p>
         </div>
       </div>
     </div>
