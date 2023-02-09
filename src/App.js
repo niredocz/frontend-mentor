74759ff0fed23cb6c992eb205e143e64493c8a74
Daffa Aldzakian F <daffaaldzakian20@gmail.com>
2023-02-09 10:06:16 +0700

    split sidebar and main-content component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
-import { Route, Routes } from 'react-router-dom'
-import ScanCode from './projects/scan-qr-code/ScanCode'
-import AdviceGenerator from './projects/advice-generator/AdviceGenerator'
+
+
+// Import Components & Projects
+import Sidebar from './layouts/Sidebar';
+import MainContent from './layouts/MainContent';
 
 
 function App() {
   return (
-    <Routes>
-      <Route path='/scan-qr-code' element={<ScanCode />} />
-      <Route path='/advice-generator' element={<AdviceGenerator />} />
-    </Routes>
+    <>
+      <Sidebar />
+      <MainContent />
+    </>
   );
 }
 
