36dbc04d8f7bd66c42b42428e8055ca6d4fba196
Daffa Aldzakian F <daffaaldzakian20@gmail.com>
2023-02-09 10:06:35 +0700

    sidebar using flowbite react component

--- src/layouts/Sidebar.jsx
+++ src/layouts/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Sidebar } from "flowbite-react";
 
-const Sidebar = () => {
+const thisSidebar = () => {
   return (
     <>
       <button
@@ -32,91 +32,38 @@ const Sidebar = () => {
         className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
         aria-label="Sidebar"
       >
-        <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
-          <ul className="space-y-2">
-            <li>
-              <Link
-                to="/"
-                className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-              >
-                <svg
-                  aria-hidden="true"
-                  className="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
-                  <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
-                </svg>
-                <span className="ml-3">Beranda</span>
-              </Link>
-            </li>
-            <li>
-              <button
-                type="button"
-                className="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                aria-controls="dropdown-example"
-                data-collapse-toggle="dropdown-example"
-              >
-                <svg
-                  aria-hidden="true"
-                  className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-                <span
-                  className="flex-1 ml-3 text-left whitespace-nowrap"
-                  sidebar-toggle-item
-                >
-                  Frontend Projects
-                </span>
-                <svg
-                  sidebar-toggle-item
-                  className="w-6 h-6"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-              </button>
+        <div className="h-full w-fit">
+          <Sidebar aria-label="Sidebar with multi-level dropdown example">
+            <div className="px-4 mb-4">
+              <h1 className="font-bold">Frontend Projects</h1>
+            </div>
+            <Sidebar.Items>
+              <Sidebar.ItemGroup>
+                <Sidebar.Item href="/">Beranda</Sidebar.Item>
+              </Sidebar.ItemGroup>
 
-              <ul id="dropdown-example" className="hidden py-2 space-y-2">
-                <li>
-                  <Link
-                    to="/scan-qr-code"
-                    className="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                  >
-                    Scan QR Code
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="/advice-generator"
-                    className="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                  >
-                    Advice Generator
-                  </Link>
-                </li>
-              </ul>
-            </li>
-          </ul>
+              <Sidebar.ItemGroup>
+                {/* <Sidebar.Collapse label="Frontend Projects">
+                  <Sidebar.Item href="/scan-qr-code">
+                    1. Scan QR Code
+                  </Sidebar.Item>
+                  <Sidebar.Item href="/advice-generator">
+                    2. Advice Generator
+                  </Sidebar.Item>
+                </Sidebar.Collapse> */}
+                <Sidebar.Item href="/scan-qr-code">
+                  1. Scan QR Code
+                </Sidebar.Item>
+                <Sidebar.Item href="/advice-generator">
+                  2. Advice Generator
+                </Sidebar.Item>
+              </Sidebar.ItemGroup>
+            </Sidebar.Items>
+          </Sidebar>
         </div>
       </aside>
     </>
   );
 };
 
-export default Sidebar;
+export default thisSidebar;
