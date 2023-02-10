05719c431dadc6cd0bf85908b60bb8c976d7a62a
Daffa Aldzakian F <daffaaldzakian20@gmail.com>
2023-02-10 17:47:59 +0700

    add tip calculator route

--- src/layouts/MainContent.jsx
+++ src/layouts/MainContent.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 // Import Project files
 import ScanCode from "../projects/scan-qr-code/ScanCode";
 import AdviceGenerator from "../projects/advice-generator/AdviceGenerator";
+import TipCalCulator from "../projects/tip-calculator/TipCalCulator";
 
 const MainContent = () => {
   return (
@@ -11,6 +12,7 @@ const MainContent = () => {
       <Routes>
         <Route path="/scan-qr-code" element={<ScanCode />} />
         <Route path="/advice-generator" element={<AdviceGenerator />} />
+        <Route path="/tip-calculator-app" element={<TipCalCulator />} />
       </Routes>
     </div>
   );
