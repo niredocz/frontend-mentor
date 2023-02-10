25b2c79a79aa76c1e1e9c4b2032cd7f0a7048521
Daffa Aldzakian F <daffaaldzakian20@gmail.com>
2023-02-10 17:48:54 +0700

    init tip calculator project

---
+++ src/projects/tip-calculator/TipCalCulator.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const TipCalCulator = () => {
+  return (
+    <>
+      <div className="App bg-[#c5e4e7]">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia
+        maxime, aperiam esse dignissimos, repellat ab sunt consequatur
+        distinctio ea vitae debitis illum nihil, magnam unde veritatis eaque.
+        Aperiam, dolore praesentium.
+      </div>
+    </>
+  );
+};
+
+export default TipCalCulator;
