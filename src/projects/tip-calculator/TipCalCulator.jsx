4d3b159585375b43fa62969170600217b02f8df3
Daffa Aldzakian F <daffaaldzakian20@gmail.com>
2023-02-13 23:21:30 +0700

    build calculator base

--- src/projects/tip-calculator/TipCalCulator.jsx
+++ src/projects/tip-calculator/TipCalCulator.jsx
@@ -1,13 +1,53 @@
 import React from "react";
+import "./TipCalculator.css";
 
 const TipCalCulator = () => {
   return (
     <>
-      <div className="App bg-[#c5e4e7]">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia
-        maxime, aperiam esse dignissimos, repellat ab sunt consequatur
-        distinctio ea vitae debitis illum nihil, magnam unde veritatis eaque.
-        Aperiam, dolore praesentium.
+      <div className="App bgTipCalculator bg-[#c5e4e7]">
+        <div className="container">
+          <div className="card mx-auto">
+            <div className="cardBody p-4">
+              <div className="flex">
+                <div className="form">
+                  <div className="bill">
+                    <h5>Bill</h5>
+                  </div>
+                  <div className="selectTip">
+                    <h5>Select Tip %</h5>
+                    <div className="flex">
+                      <button className="w-1/3" type="button">
+                        5%
+                      </button>
+                      <button type="button">10%</button>
+                      <button type="button">15%</button>
+                      <button type="button">25%</button>
+                      <button type="button">50%</button>
+                      <input type="text" placeholder="Custom" />
+                    </div>
+                  </div>
+                </div>
+                <div className="result">
+                  <div className="wrap">
+                    <h4>
+                      Tip Amount <br /> <span>/ person</span>
+                    </h4>
+                    <span>$ 0.00</span>
+                  </div>
+                  <div className="wrap">
+                    <h4>
+                      Total <br /> <span>/ person</span>
+                    </h4>
+                    <span>$ 0.00</span>
+                  </div>
+                  <button className="btnReset" type="button">
+                    RESET
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
