c5e8fbcdf70db8e654eacabf33c37af418feffbf
Daffa Aldzakian F <daffaaldzakian20@gmail.com>
2023-02-20 08:54:09 +0700

    add home dashboard component

---
+++ src/layouts/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="App bgHome bg-[#74b9ff]">
+      <div className="card">
+        <h1 className="text-4xl">Kumpulan project-project dari <br /> <a href="https://www.frontendmentor.io/">Frontend Mentor</a></h1>
+
+		<div className="flex">
+			<a href="#"></a>
+		</div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
