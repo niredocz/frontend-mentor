d14bfccbde9db2245a9aab5a81a6a1a0d35a180f
Daffa Aldzakian F <daffaaldzakian20@gmail.com>
2023-02-23 09:56:59 +0700

    add home component

--- src/layouts/MainContent.jsx
+++ src/layouts/MainContent.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 // Import Project files
+import Home from "./Home";
 import ScanCode from "../projects/scan-qr-code/ScanCode";
 import AdviceGenerator from "../projects/advice-generator/AdviceGenerator";
 import TipCalCulator from "../projects/tip-calculator/TipCalCulator";
@@ -10,6 +11,7 @@ const MainContent = () => {
   return (
     <div className="sm:ml-64">
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/scan-qr-code" element={<ScanCode />} />
         <Route path="/advice-generator" element={<AdviceGenerator />} />
         <Route path="/tip-calculator-app" element={<TipCalCulator />} />
