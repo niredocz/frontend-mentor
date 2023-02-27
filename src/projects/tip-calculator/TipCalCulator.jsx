6f70a398b165da31761be5f0ec0dbd96cd6faa07
Daffa Aldzakian F <daffaaldzakian20@gmail.com>
2023-02-27 12:03:50 +0700

    handleRadioChange, switch case based on radio value

--- src/projects/tip-calculator/TipCalCulator.jsx
+++ src/projects/tip-calculator/TipCalCulator.jsx
@@ -6,20 +6,26 @@ const TipCalCulator = () => {
   const [billValue, setBillValue] = useState(0.0);
   const [customBillValue, setCustomBillValue] = useState(null);
   const [peopleValue, setPeopleValue] = useState(1);
-  const [isRadioChecked, setIsRadioChecked] = useState(false);
+  const [isRadioChecked, setIsRadioChecked] = useState(null);
 
-  function handleRadioChange() {
-    setIsRadioChecked(true);
+  function handleRadioChange(option) {
+    if (isRadioChecked === option) {
+      setIsRadioChecked(null);
+    } else {
+      setIsRadioChecked(option);
+      setCustomBillValue(null);
+      document.getElementById("custom-input").value = "";
+    }
   }
-  
+
   const handleBillChange = (e) => {
     setBillValue(e.target.value);
   };
-  
+
   const handleCustomBillChange = (e) => {
     setCustomBillValue(e.target.value);
   };
-  
+
   const handlePeopleChange = (e) => {
     if (e.target.value != 0) {
       setPeopleValue(e.target.value);
@@ -27,50 +33,79 @@ const TipCalCulator = () => {
       setPeopleValue(1);
     }
   };
-  
-  const countTipAmount = () => {
+
+  const countTipAmount = (e) => {
     let a = (parseFloat(billValue) * 1) / parseFloat(peopleValue);
-  
+    let radioButtons = document.getElementsByName("bills");
+
+    // loop through the radio buttons to find the selected one
+    let selectedRadioButton;
+    for (let i = 0; i < radioButtons.length; i++) {
+      if (radioButtons[i].checked) {
+        selectedRadioButton = radioButtons[i].value;
+        break;
+      }
+    }
+
+    let selectedValue = selectedRadioButton;
+
     if (customBillValue > 0) {
       a =
         (parseFloat(billValue) * (customBillValue / 100)) /
         parseFloat(peopleValue);
+    } else if (isRadioChecked != null) {
+      switch (selectedValue) {
+        case "5":
+          a = (parseFloat(billValue) * (5 / 100)) / parseFloat(peopleValue);
+          break;
+        case "10":
+          a = (parseFloat(billValue) * (10 / 100)) / parseFloat(peopleValue);
+          break;
+        case "15":
+          a = (parseFloat(billValue) * (15 / 100)) / parseFloat(peopleValue);
+          break;
+        case "25":
+          a = (parseFloat(billValue) * (25 / 100)) / parseFloat(peopleValue);
+          break;
+        case "50":
+          a = (parseFloat(billValue) * (50 / 100)) / parseFloat(peopleValue);
+          break;
+        default:
+          a = (parseFloat(billValue) * 1) / parseFloat(peopleValue);
+      }
     }
-  
+
     let roundedNum = Math.round(a * 100) / 100;
-  
+
     return roundedNum;
   };
-  
+
   const countTotalPerPerson = () => {
     let a = parseFloat(billValue) / parseFloat(peopleValue);
     let b = customBillValue;
     let c = countTipAmount();
-  
-    if (b > 0) {
+
+    if (b > 0 || isRadioChecked != null) {
       a = (parseFloat(billValue) + c) / parseFloat(peopleValue);
     }
-  
+
     let roundedNum = Math.round(a * 100) / 100;
-  
+
     return roundedNum;
   };
-  
+
   const tipAmount = countTipAmount();
   const totalPerPerson = countTotalPerPerson();
-  
+
   const handleReset = () => {
     setBillValue(0.0);
     setCustomBillValue(null);
     setPeopleValue(1);
+    setIsRadioChecked(false);
     document.getElementById("bill-input").value = "";
     document.getElementById("custom-input").value = "";
     document.getElementById("number-input").value = 1;
   };
-
-  const tes = document.getElementsByClassName('peer')
-
-  // if (tes === '')
 
   return (
     <>
@@ -91,6 +126,7 @@ const TipCalCulator = () => {
                           d="M6.016 16.328v-1.464c1.232-.08 2.22-.444 2.964-1.092.744-.648 1.116-1.508 1.116-2.58v-.144c0-.992-.348-1.772-1.044-2.34-.696-.568-1.708-.932-3.036-1.092V4.184c.56.144 1.012.4 1.356.768.344.368.516.816.516 1.344v.288h1.824v-.432c0-.448-.088-.876-.264-1.284a3.783 3.783 0 00-.744-1.116A4.251 4.251 0 007.54 2.9a5.324 5.324 0 00-1.524-.492V.872H4.288V2.36a5.532 5.532 0 00-1.416.324c-.448.168-.84.392-1.176.672-.336.28-.604.616-.804 1.008-.2.392-.3.844-.3 1.356v.144c0 .96.316 1.708.948 2.244.632.536 1.548.884 2.748 1.044v3.912c-.704-.16-1.248-.472-1.632-.936-.384-.464-.576-1.08-.576-1.848v-.288H.256v.576c0 .464.08.924.24 1.38.16.456.404.88.732 1.272.328.392.744.728 1.248 1.008s1.108.476 1.812.588v1.512h1.728zM4.288 7.424c-.688-.128-1.164-.332-1.428-.612-.264-.28-.396-.644-.396-1.092 0-.464.176-.832.528-1.104.352-.272.784-.448 1.296-.528v3.336zm1.728 5.712V9.344c.768.128 1.328.328 1.68.6.352.272.528.688.528 1.248 0 .544-.196.984-.588 1.32-.392.336-.932.544-1.62.624z"
                         />
                       </svg>
+
                       <input
                         type="number"
                         id="bill-input"
@@ -100,38 +136,94 @@ const TipCalCulator = () => {
                       />
                     </div>
                   </div>
+
                   <div className="selectTip mb-5">
                     <h5 className="text-left font-semibold text-lg mb-2">
                       Select Tip %
                     </h5>
                     <ul className="grid md:grid-cols-3 md:gap-3">
                       <li>
-                        <input type="radio" id="tip-5" name="hosting" value="5" className="hidden peer" checked={isRadioChecked} onChange={handleRadioChange} required />
-                        <label htmlFor="tip-5" className="block cursor-pointer w-full py-3 px-2 rounded-md font-semibold text-2xl text-white bg-[#00474b] hover:bg-[#9fe8df] hover:text-green-900 peer-checked:bg-[#9fe8df] peer-checked:text-green-900">                           
+                        <input
+                          type="radio"
+                          id="tip-5"
+                          name="bills"
+                          value="5"
+                          className="hidden peer"
+                          checked={isRadioChecked === "tip-5"}
+                          onClick={() => handleRadioChange("tip-5")}
+                          required
+                        />
+                        <label
+                          htmlFor="tip-5"
+                          className="block cursor-pointer w-full py-3 px-2 rounded-md font-semibold text-2xl text-white bg-[#00474b] hover:bg-[#9fe8df] hover:text-green-900 peer-checked:bg-[#9fe8df] peer-checked:text-green-900">
                           5%
                         </label>
                       </li>
                       <li>
-                        <input type="radio" id="tip-10" name="hosting" value="10" className="hidden peer" checked={isRadioChecked} onChange={handleRadioChange} required />
-                        <label htmlFor="tip-10" className="block cursor-pointer w-full py-3 px-2 rounded-md font-semibold text-2xl text-white bg-[#00474b] hover:bg-[#9fe8df] hover:text-green-900 peer-checked:bg-[#9fe8df] peer-checked:text-green-900">                           
+                        <input
+                          type="radio"
+                          id="tip-10"
+                          name="bills"
+                          value="10"
+                          className="hidden peer"
+                          checked={isRadioChecked === "tip-10"}
+                          onClick={() => handleRadioChange("tip-10")}
+                          required
+                        />
+                        <label
+                          htmlFor="tip-10"
+                          className="block cursor-pointer w-full py-3 px-2 rounded-md font-semibold text-2xl text-white bg-[#00474b] hover:bg-[#9fe8df] hover:text-green-900 peer-checked:bg-[#9fe8df] peer-checked:text-green-900">
                           10%
                         </label>
                       </li>
                       <li>
-                        <input type="radio" id="tip-15" name="hosting" value="15" className="hidden peer" checked={isRadioChecked} onChange={handleRadioChange} required />
-                        <label htmlFor="tip-15" className="block cursor-pointer w-full py-3 px-2 rounded-md font-semibold text-2xl text-white bg-[#00474b] hover:bg-[#9fe8df] hover:text-green-900 peer-checked:bg-[#9fe8df] peer-checked:text-green-900">                           
+                        <input
+                          type="radio"
+                          id="tip-15"
+                          name="bills"
+                          value="15"
+                          className="hidden peer"
+                          checked={isRadioChecked === "tip-15"}
+                          onClick={() => handleRadioChange("tip-15")}
+                          required
+                        />
+                        <label
+                          htmlFor="tip-15"
+                          className="block cursor-pointer w-full py-3 px-2 rounded-md font-semibold text-2xl text-white bg-[#00474b] hover:bg-[#9fe8df] hover:text-green-900 peer-checked:bg-[#9fe8df] peer-checked:text-green-900">
                           15%
                         </label>
                       </li>
                       <li>
-                        <input type="radio" id="tip-25" name="hosting" value="25" className="hidden peer" checked={isRadioChecked} onChange={handleRadioChange} required />
-                        <label htmlFor="tip-25" className="block cursor-pointer w-full py-3 px-2 rounded-md font-semibold text-2xl text-white bg-[#00474b] hover:bg-[#9fe8df] hover:text-green-900 peer-checked:bg-[#9fe8df] peer-checked:text-green-900">                           
+                        <input
+                          type="radio"
+                          id="tip-25"
+                          name="bills"
+                          value="25"
+                          className="hidden peer"
+                          checked={isRadioChecked === "tip-25"}
+                          onClick={() => handleRadioChange("tip-25")}
+                          required
+                        />
+                        <label
+                          htmlFor="tip-25"
+                          className="block cursor-pointer w-full py-3 px-2 rounded-md font-semibold text-2xl text-white bg-[#00474b] hover:bg-[#9fe8df] hover:text-green-900 peer-checked:bg-[#9fe8df] peer-checked:text-green-900">
                           25%
                         </label>
                       </li>
                       <li>
-                        <input type="radio" id="tip-50" name="hosting" value="50" className="hidden peer" checked={isRadioChecked} onChange={handleRadioChange} required />
-                        <label htmlFor="tip-50" className="block cursor-pointer w-full py-3 px-2 rounded-md font-semibold text-2xl text-white bg-[#00474b] hover:bg-[#9fe8df] hover:text-green-900 peer-checked:bg-[#9fe8df] peer-checked:text-green-900">                           
+                        <input
+                          type="radio"
+                          id="tip-50"
+                          name="bills"
+                          value="50"
+                          className="hidden peer"
+                          checked={isRadioChecked === "tip-50"}
+                          onClick={() => handleRadioChange("tip-50")}
+                          required
+                        />
+                        <label
+                          htmlFor="tip-50"
+                          className="block cursor-pointer w-full py-3 px-2 rounded-md font-semibold text-2xl text-white bg-[#00474b] hover:bg-[#9fe8df] hover:text-green-900 peer-checked:bg-[#9fe8df] peer-checked:text-green-900">
                           50%
                         </label>
                       </li>
@@ -148,7 +240,7 @@ const TipCalCulator = () => {
                     </ul>
                   </div>
 
-                  <div className="numberOfPeople mb-5">
+                  <div className="numberOfPeople">
                     <h5 className="text-left font-semibold text-lg mb-2">
                       Number of People
                     </h5>
@@ -194,7 +286,7 @@ const TipCalCulator = () => {
                     </div>
                   </div>
 
-                  <ResetButton click={handleReset}/>
+                  <ResetButton click={handleReset} />
                 </div>
               </div>
             </div>
