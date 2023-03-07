447449e2a81dc0020d8242beec382b7adf9d22f2
Daffa Aldzakian F <daffaaldzakian20@gmail.com>
2023-03-07 14:25:33 +0700

    add content on flagCard component

--- src/projects/rest-countries/FlagCard.jsx
+++ src/projects/rest-countries/FlagCard.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 
-const FlagCard = ({src, alt, title}) => {
+const FlagCard = ({ src, alt, countryName, population, region, capital }) => {
   return (
-    <div className="flagCard bg-white rounded-md shadow-md text-center">
+    <div className="flagCard bg-white rounded-md shadow-md">
       <img className="imgCard" src={src} alt={alt} />
 
       <div className="content p-4">
-        <h1 className="text-lg">{title}</h1>
+        <h1 className="text-lg font-bold mb-2">{countryName}</h1>
+        <h5>
+          <span className="font-bold">Population:</span> {population}
+        </h5>
+        <h5>
+          <span className="font-bold">Region:</span> {region}
+        </h5>
+        <h5>
+          <span className="font-bold">Capital:</span> {capital}
+        </h5>
       </div>
     </div>
   );
